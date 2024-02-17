408a11aee16b68f7b4f559ed6fb29f49b4587608
Nirankar <niranakr.d.mandal@gmail.com>
2024-02-17 12:51:37 +0530

    task completed

--- myjavafile.java
+++ myjavafile.java
@@ -1,2 +1,11 @@
 This is my java file
+<<<<<<< Updated upstream
 this is my manager work
+=======
+1st day of java line of code completed
+2nd day java line of code completed
+3rd day java line of code completed
+4th day java line of code completed
+5th day java line of code completed
+
+>>>>>>> Stashed changes
